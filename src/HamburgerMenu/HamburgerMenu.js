60596261a8d8be815085485bfc30da78af4c626d
Danyeal Mateen <danyeal.mateen@hotmail.com>
2023-05-25 16:49:41 +0200

    Hamburger Menu Funktion Klar

--- src/HamburgerMenu/HamburgerMenu.js
+++ src/HamburgerMenu/HamburgerMenu.js
@@ -1,15 +1,30 @@
 import './HamburgerMenu.css';
+// kolla vad detta gör eller heter
+import React, { useState } from 'react';
 
 export function HamburgerMenu() {
 
+    const [isOpen, setIsOpen] = useState(false)
 
+    console.log(isOpen)
+    console.log(!isOpen)
+
+    function toggleMenu() {
+        setIsOpen(!isOpen)
+    }
 
     return (
         <>
-            <button className="hamburgerMenuBtn">X</button>
-            <div className="link">link</div>
-            <div className="link">link</div>
-            <div className="link">link</div>
+            <button className="hamburgerMenuBtn" onClick={toggleMenu}>
+                {isOpen ? "X" : "☰"}
+            </button>
+            {isOpen && (
+                <>
+                    <div className="link">link</div>
+                    <div className="link">link</div>
+                    <div className="link">link</div>
+                </>
+            )}
         </>
     )
-}
+};
