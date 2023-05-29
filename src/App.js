a71ea195a1c374a3e8a06d98614595d42e818838
Danyeal Mateen <danyeal.mateen@hotmail.com>
2023-05-29 15:16:04 +0200

    Switch removed from App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css"
 import { Main } from "./Main/Main";
-import { BrowserRouter as Router, Route, Routes, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AboutMe } from "./AboutMe/AboutMe";
 
 function App() {
