1e273d1ca4ebfa2928968531f0398b06c8d9a133
Danyeal Mateen <danyeal.mateen@hotmail.com>
2023-05-31 10:03:04 +0200

    header and main done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./App.css"
 import { Main } from "./Main/Main";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { AboutMe } from "./AboutMe/AboutMe";
 
 function App() {
   return (
 
     <Router>
       <Routes>
-        <Route path="/aboutme" element={<AboutMe />} />
+        {/* <Route path="/aboutme" element={<AboutMe />} /> */}
         <Route path="/" element={<Main />} />
       </Routes>
     </Router>
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,20 +1,52 @@
 import "./Main.css";
-import placeholder from "../Images/placeholder.jpg"
-import { HamburgerMenu } from "../HamburgerMenu/HamburgerMenu";
+import mainlogo from "../Images/mainlogo.png"
+import html from "../Logos/html.png"
+import css from "../Logos/css.png"
+import js from "../Logos/js.png"
+import react from "../Logos/react.png"
+import git from "../Logos/git.png"
+import firebase from "../Logos/firebase.png"
+import jira from "../Logos/jira.png"
+import scrum from "../Logos/scrum.png"
+import ts from "../Logos/ts.png";
 import { Header } from "../Header/Header";
-import { CVLinkBar } from "../CVLinkBar/CVLinkBar";
-import { Footer } from "../Footer/Footer";
-import { InfoBoxes } from "../InfoBoxes/InfoBoxes.js";
 
 export function Main() {
     return (
         <>
             <Header />
-        
-            {/* <HamburgerMenu /> */}
-            {/* <CVLinkBar /> */}
-            {/* <InfoBoxes /> */}
-            {/* <Footer /> */}
+            <div className="contentContainer">
+                <div className="textContent">
+                    <h1>Hi there, I'm <span className="danySpan">Dany</span></h1>
+                    <h3>A Front End developer in Malmö, Sweden.</h3>
+                    <hr />
+                </div>
+
+                <div className="logoContainer">
+                    <img className="mainlogo" src={mainlogo} />
+                    <hr className="logoHr" />
+                </div>
+
+                <div className="skillsetContainer">
+                    <img src={html} className="html" />
+                    <hr className="hr" />
+                    <img src={css} className="css" />
+                    <hr className="hr" />
+                    <img src={js} className="js" />
+                    <hr className="hr" />
+                    <img src={react} className="react" />
+                    <hr className="hr" />
+                    <img src={git} className="git" />
+                    <hr className="hr" />
+                    <img src={firebase} className="firebase" />
+                    <hr className="hr" />
+                    <img src={jira} className="jira" />
+                    <hr className="hr" />
+                    <img src={scrum} className="scrum" />
+                    <hr className="hr" />
+                    <img src={ts} className="ts" />
+                </div>
+            </div>
         </>
     )
 }
