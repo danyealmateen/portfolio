72441e7c40000b4288f3f4c07bad68a207038e22
Danyeal Mateen <danyeal.mateen@hotmail.com>
2023-06-05 15:45:19 +0200

    Added the linkedin logo

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,6 @@
 import "./Header.css"
 import logo from "../Images/logo.png";
+import linkedin from "../Images/linkedin.png";
 
 export function Header() {
     return (
@@ -9,7 +10,7 @@ export function Header() {
                 <img className="logo" src={logo} />
                 <h2 className="headerTitle">DANYWEBDEV</h2>
             </div>
-            <h3 className="socialLinks">LinkedIn</h3>
+            <a className="socialLinks" href=""><img className="linkedin" src={linkedin} /></a>
             <h3 className="socialLinks">Projects</h3>
         </div>
     )
